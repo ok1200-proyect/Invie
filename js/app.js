d257247f07dbd9399d52d1984c78306f607b330f
Kevin Toro <kctoro74@gmail.com>
2018-06-03 14:31:42 -0500

    Maquetación de Invie
    Maquetación de Invie

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const $filtrosToggle = $('#BotonCollapse')
+$filtrosToggle.click((ev) => {
+  ev.preventDefault()
+
+  const $i = $filtrosToggle.find('i.fa')
+  const isDown = $i.hasClass('fa-chevron-down')
+  if (isDown) {
+    $i.removeClass('fa-chevron-down').addClass('fa-chevron-up')
+  } else {
+    $i.removeClass('fa-chevron-up').addClass('fa-chevron-down')
+  }
+})
